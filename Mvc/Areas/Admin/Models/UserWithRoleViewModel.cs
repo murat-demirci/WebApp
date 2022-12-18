e44bd112336ea7cc60b8f92c4d1ef2d701cc9c2f
murat-demrici <ddemircimuratt@gmail.com>
2022-12-18 17:11:52 +0300

    _layoutSideBar i ViewComponent yapma

---
+++ Mvc/Areas/Admin/Models/UserWithRoleViewModel.cs
@@ -0,0 +1,10 @@
+﻿using Entities.Concrete;
+
+namespace Mvc.Areas.Admin.Models
+{
+    public class UserWithRoleViewModel
+    {
+        public User User { get; set; }
+        public IList<string> Roles { get; set; }
+    }
+}
---
+++ Mvc/Areas/Admin/ViewComponents/AdminMenuViewComponent.cs
@@ -0,0 +1,39 @@
+﻿using Entities.Concrete;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.ViewComponents;
+using Mvc.Areas.Admin.Models;
+
+namespace Mvc.Areas.Admin.ViewComponents
+{
+    [Area("Admin")]
+    //menuyu dinamik hale getirmek icin yapilir
+    //menu diger bilesenlerden bagimsiz olarak calisacak
+    //controller gibi dusunulebilir
+    public class AdminMenuViewComponent : ViewComponent
+    {
+
+        private readonly UserManager<User> _userManager;
+        public AdminMenuViewComponent(UserManager<User> userManager)
+        {
+            _userManager = userManager;
+            //view icinde model kuillnilir, model eklenmeli
+        }
+        //her bir component bi adet invoke metota sahip olmali
+        public ViewViewComponentResult Invoke()
+        {
+            //controller da view return eder gibi, oncesinde model kullanma/ islem yapma imkani sunar
+            var user = _userManager.GetUserAsync(HttpContext.User).Result;
+            //httpcontext.user hangi kullanici login ise onu getirir.
+            var roles = _userManager.GetRolesAsync(user).Result;
+            return View(new UserWithRoleViewModel
+            {
+                User = user,
+                Roles = roles
+            });
+        }
+        //sayfa yonlendirmesi icin, shared icine Components klasoru
+        //sonra AdminMenu klasoru acilir
+        //icerisinde Default.cshtml sayfasi bulunmali
+    }
+}
